6b2de097734fc2ac86a929b21023923ba6a20b5d
tonousa <tonousa@hotmail.com>
2020-03-22 18:52:30 -0500

    paging and unit test

--- SportsStore.UnitTest/UnitTest1.cs
+++ SportsStore.UnitTest/UnitTest1.cs
@@ -1,5 +1,13 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.WebUI.Controllers;
+using Moq;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SportsStore.UnitTest
 {
@@ -7,8 +15,30 @@ namespace SportsStore.UnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Paginate()
         {
+            // arrange
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] 
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
+            });
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            // act
+            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+
+            // assert
+            Product[] prodArray = result.ToArray();
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.IsTrue(prodArray[0].Name == "P4");
+            Assert.IsTrue(prodArray[1].Name == "P5");
         }
     }
 }
