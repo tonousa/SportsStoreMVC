dff35e0e9530f34131194efcbfa7324035e9a3ff
tonousa <tonousa@hotmail.com>
2020-03-23 15:11:34 -0500

    Helper Method

---
+++ SportsStore.WebUI/HtmlHelpers/PagingHelpers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.HtmlHelpers
+{
+    public class PagingHelpers
+    {
+
+    }
+}
