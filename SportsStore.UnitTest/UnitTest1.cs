2f33f2383950d9beb6a5a58814ee6bbcb1a18bf3
tonousa <tonousa@hotmail.com>
2020-03-28 11:47:40 -0500

    repo.Products.Select(c => c.Category).Distinct()

--- SportsStore.UnitTest/UnitTest1.cs
+++ SportsStore.UnitTest/UnitTest1.cs
@@ -124,5 +124,30 @@ namespace SportsStore.UnitTest
             Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
             Assert.IsTrue(result[1].Name == "P4" && result[1].Category == "Cat2");
         }
+
+        [TestMethod]
+        public void Can_Create_Categories()
+        {
+            // Arrange
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+                new Product {ProductID = 2, Name = "P2", Category = "Apples"},
+                new Product {ProductID = 3, Name = "P3", Category = "Plums"},
+                new Product {ProductID = 4, Name = "P4", Category = "Oranges"},
+            });
+
+            NavController target = new NavController(mock.Object);
+
+            // Act
+            string[] results = ((IEnumerable<string>)target.Menu().Model).ToArray();
+
+            // Assert
+            Assert.AreEqual(results.Length, 3);
+            Assert.AreEqual(results[0], "Apples");
+            Assert.AreEqual(results[1], "Oranges");
+            Assert.AreEqual(results[2], "Plums"); 
+        }
     }
 }
