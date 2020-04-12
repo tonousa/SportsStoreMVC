7c1dff2c80df4f2771c4937493d7a38eb2e128d2
tonousa <tonousa@hotmail.com>
2020-04-12 11:28:27 -0500

    repo.Products.FirstOrDefault(p => p.Id = prodId)

---
+++ SportsStore.UnitTest/AdminTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore.UnitTest
+{
+    [TestClass]
+    public class AdminTests
+    {
+        [TestMethod]
+        public void Index_Contains_AllProducts()
+        {
+            //Arrange
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+            });
+
+            AdminController target = new AdminController(mock.Object);
+
+            // Act
+            Product[] result = ((IEnumerable<Product>)target.Index().ViewData.Model).ToArray();
+
+            // Assert
+            Assert.AreEqual(result.Length, 3);
+            Assert.AreEqual(result[0].Name, "P1");
+            Assert.AreEqual(result[1].Name, "P2");
+            Assert.AreEqual(result[2].Name, "P3");
+        }
+    }
+}
