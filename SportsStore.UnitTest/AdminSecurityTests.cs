5038e4fddade073dcc8935e3152355fc7fd6f0bb
tonousa <tonousa@hotmail.com>
2020-04-21 11:27:32 -0500

    mock.Setup(m => m.Method()).Returns(...

---
+++ SportsStore.UnitTest/AdminSecurityTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using SportsStore.WebUI.Controllers;
+using Moq;
+using SportsStore.WebUI.Infrastructure.Abstract;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.UnitTest
+{
+    [TestClass]
+    public class AdminSecurityTests
+    {
+        [TestMethod]
+        public void Can_Login_Valid_Credentials()
+        {
+            Mock<IAuthProvider> mock = new Mock<IAuthProvider>();
+            mock.Setup(m => m.Authenticate("admin","secret")).Returns(true);
+
+            LoginViewModel model = new LoginViewModel
+            {
+                UserName = "admin",
+                Password = "secret"
+            };
+
+            AccountController target = new AccountController(mock.Object);
+
+            ActionResult result = target.Login(model, "/MyUrl");
+
+            Assert.IsInstanceOfType(result, typeof(RedirectResult));
+            Assert.AreEqual("/MyUrl", ((RedirectResult)result).Url);
+        }
+    }
+}
